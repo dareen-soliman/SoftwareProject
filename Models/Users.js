b92fd5376640f9960298ad685b04ec61e7a489e0
FaroukCoder <yosef.farok@student.giu-uni.de>
2025-03-06 23:22:06 +0400

    "added users schema"

---
+++ Models/Users.js
@@ -0,0 +1,15 @@
+const { type } = require('os');
+const mongoose = require('mongoose');
+const { timeStamp } = require('console');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    profilePicture: { type: String }, 
+    password: { type: String, required: true },
+    age:{ type: Number, required: true },
+    role: { type: String, enum: ['standard', 'organizer', 'admin'], default: 'standard' },
+    createdAt: { type: Date, default: Date.now }
+  });
+
+module.exports = mongoose.model('User', userSchema);

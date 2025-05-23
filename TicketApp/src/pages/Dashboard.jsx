18c6abebb20fc22c0c9c70bc60ea5dba0b7ac946
lusgad <lama.hassan@student.giu-uni.de>
2025-05-23 17:37:05 +0300

    Update Dashboard.jsx

--- TicketApp/src/pages/Dashboard.jsx
+++ TicketApp/src/pages/Dashboard.jsx
@@ -8,11 +8,14 @@ function Dashboard() {
   const handleGoToProfile = () => {
     navigate("/profile");
   };
-    const handleGoToMyEvents = () => {
+    const handleGoToAllEvents = () => {
     navigate("/events");
   };
    const handleGoToMyBookings = () => {
     navigate("/user-bookings");
+  };
+    const handleGoToMyEvents = () => {
+    navigate("/my-events");
   };
  
 
@@ -21,8 +24,9 @@ function Dashboard() {
       <h2>Welcome to Your Dashboard!</h2>
       <p>You are now logged in.</p>
       <button onClick={handleGoToProfile}>Go to Profile</button>
-       <button onClick={handleGoToMyEvents}>View All Events</button>
+       <button onClick={handleGoToAllEvents}>View All Events</button>
         <button onClick={handleGoToMyBookings}>View My Bookings</button>
+        <button onClick={handleGoToMyEvents}>View My Events</button>
     </div>
   );
 }
